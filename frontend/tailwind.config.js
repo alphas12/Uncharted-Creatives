3f2763a312178102d56ccfaaef736c7782e2803d
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-12-15 20:39:51 +0800

    [Alniño] [Download auto-prefixer]

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      extend: {
+          colors: {
+              dominant: '#FAF7F0',  // Light Greyish Tone
+              primary: '#4A4947',   // Medium Dark
+              secondary: '#D8D2C2',
+              accent: '#B17457'      // Background Light Color
+           },
+
+          fontFamily: {
+              heading: ['Poppins', 'sans-serif'], // For headers
+              body: ['Open Sans', 'sans-serif'],      // For body text
+          },
+      },
+  },
+  plugins: [],
+}
